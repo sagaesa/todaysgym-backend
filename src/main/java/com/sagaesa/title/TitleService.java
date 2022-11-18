dd1c22b6e5e38624e74f4f9434f1a5037a729dd4
yoonsj970407 <yoonsj970407@gmail.com>
2022-11-18 15:42:43 +0900

    Feat: title 얻는 로직 구현
    일단 기록한 운동이 10개 이상인 경우에, title을 얻도록 구현. 기록한 운동이 10개 이상인 경우 얻는 title의 id가 1이라고 가정하고 구현

--- src/main/java/com/sagaesa/title/TitleService.java
+++ src/main/java/com/sagaesa/title/TitleService.java
@@ -1,5 +1,6 @@
 package com.sagaesa.title;
 
+import com.sagaesa.record.RecordService;
 import com.sagaesa.title.dto.TitleFindDto;
 import com.sagaesa.user.User;
 import com.sagaesa.user.UserRepository;
@@ -19,6 +20,7 @@ public class TitleService {
     private final TitleRepository titleRepository;
     private final UserRepository userRepository;
     private final UserTitleRepository userTitleRepository;
+    private final RecordService recordService;
 
     public List<TitleFindDto> findAll(Long userId) {
         User user = userRepository.findById(userId).orElseThrow();
@@ -39,5 +41,16 @@ public class TitleService {
         }
 
         return titleFindDtos;
+    }
+
+    public void titleGet(Long userId) { // 운동을 기록했을 때, 조건을 달성하면 title을 얻는 로직
+        if(recordService.findAll(userId).size() >= 10) { // 기록한 운동이 10개 이상이면 ...
+            UserTitle userTitle = UserTitle.builder()
+                    .titleId(titleRepository.findById(1L).get())
+                    .userId(userRepository.findById(userId).get())
+                    .build();
+
+            userTitleRepository.save(userTitle);
+        }
     }
 }
