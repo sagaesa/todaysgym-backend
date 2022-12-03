aa0d381f0e388f8acd193b771e1e0d07e3781951
yoonsj970407 <yoonsj970407@gmail.com>
2022-12-04 00:18:57 +0900

    Feat: 게시글 등록하는 기능 및 전체 조회 기능 구현

--- src/main/java/com/sagaesa/post/PostController.java
+++ src/main/java/com/sagaesa/post/PostController.java
@@ -1,12 +1,44 @@
 package com.sagaesa.post;
 
+import com.sagaesa.post.dto.PostCreateDto;
+import com.sagaesa.post.dto.PostFindDto;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/posts")
 public class PostController {
     final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity recordPost(@RequestBody Map<String, String> recordRequest) {
+        PostCreateDto postCreateDto = PostCreateDto.builder()
+                .userId(Long.valueOf((recordRequest.get("userId"))))
+                .categoryId(Long.valueOf(recordRequest.get("categoryId")))
+                .date(Date.valueOf(recordRequest.get("date")))
+                .title(recordRequest.get("title"))
+                .content(recordRequest.get("content"))
+                .build();
+
+        postService.recordPost(postCreateDto);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("lists")
+    public ResponseEntity<List<PostFindDto>> recordFineAll(@RequestParam("userId") Long userId) {
+        return new ResponseEntity<>(postService.findAll(userId), HttpStatus.OK);
+    }
+
+
 }
--- src/main/java/com/sagaesa/post/PostService.java
+++ src/main/java/com/sagaesa/post/PostService.java
@@ -1,10 +1,46 @@
 package com.sagaesa.post;
 
+import com.sagaesa.post.dto.PostCreateDto;
+import com.sagaesa.post.dto.PostFindDto;
+import com.sagaesa.user.User;
+import com.sagaesa.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public void recordPost(PostCreateDto postCreateDto) {
+        Post post = new Post();
+
+        Optional<User> user = userRepository.findById(postCreateDto.getUserId());
+        post.save(postCreateDto.getDate(), postCreateDto.getTitle(), postCreateDto.getContent(),
+                user.get(), user.get().getCategoryId());
+
+        postRepository.save(post);
+    }
+
+    public List<PostFindDto> findAll(Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        List<Post> posts = postRepository.findAllByCategoryId(user.get().getCategoryId());
+        List<PostFindDto> postFindDtos = new ArrayList<>();
+
+        for (Post post : posts) {
+            PostFindDto postFindDto = PostFindDto.builder()
+                    .date(post.getDate())
+                    .title(post.getTitle())
+                    .build();
+
+            postFindDtos.add(postFindDto);
+        }
+
+        return postFindDtos;
+    }
 }
